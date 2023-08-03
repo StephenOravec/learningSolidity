80ba376547ef8bf06c3d98da6c17480ef7e62866
Stephen Oravec <stephen@oravec.io>
2023-08-03 18:47:18 -0400

    Add hello-world.sol

---
+++ contracts/hello-world.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+contract HelloWorld {
+
+    function SayHello() external pure returns (string memory) {
+        return "Hello, World!";
+    }
+
+}
