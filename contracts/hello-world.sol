ca4559806559537667c9be89482f36b9def3d486
Stephen Oravec <stephen@oravec.io>
2023-08-03 18:53:33 -0400

    Add comment with exercise url

--- contracts/hello-world.sol
+++ contracts/hello-world.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 
+// Exercise: https://docs.base.org/base-camp/docs/contracts-and-basic-functions/hello-world-step-by-step
+
 pragma solidity 0.8.17;
 
 contract HelloWorld {
