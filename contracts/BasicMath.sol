af06cb086c6298b87a7c15c8d654f912d905c888
Stephen Oravec <stephen@oravec.io>
2023-08-06 01:09:03 -0400

    Update BasicMath.sol

--- contracts/BasicMath.sol
+++ contracts/BasicMath.sol
@@ -4,11 +4,30 @@ pragma solidity 0.8.17;
 
 contract BasicMath {
 
-    function adder(uint _a, uint _b) public {
+    function adder(uint _a, uint _b) public pure returns (uint sum, bool error) {
+         {
+            sum = _a + _b;
+            if (sum >= _a && sum >= _b) {
+                error = false;
+            }
+            else {
+                sum = 0;
+                error = true;
+            }
+        }
 
     }
 
-    function subtractor(uint _a, uint _b) public {
+    function subtractor(uint _a, uint _b) public pure returns (uint difference, bool error) {
+        {
+            if (_a >= _b) {
+                difference = _a - _b;
+                error = false;
+            } else {
+                difference = 0;
+                error = true;
+            }
+        }
 
     }
     
