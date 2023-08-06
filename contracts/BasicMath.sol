9eaa1a2d7d693aa1cb974264433bde4bda755c3e
Stephen Oravec <stephen@oravec.io>
2023-08-05 23:33:57 -0400

    Update gitignore and BasicMath

---
+++ contracts/BasicMath.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT 
+// Exercise: https://docs.base.org/base-camp/docs/contracts-and-basic-functions/basic-functions-exercise
+pragma solidity 0.8.17;
+
+contract BasicMath {
+    
+}
